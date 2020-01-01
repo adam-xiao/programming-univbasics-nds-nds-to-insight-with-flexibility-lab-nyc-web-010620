05da22d615cd9b583c41b8f0e1e0258b6a182f55
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-01 22:11:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,7 +35,7 @@ end
 # Your code after this point
 
 ss = movie_with_director_name(directors_database[0][:name], directors_database[0][:movies][0])
-puts ss[0]
+puts ss
 
 def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
