6175cfbef0bd4b3b8c9e649b8acd89b676c9d0b7
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-01 22:00:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,7 +31,8 @@ def movie_with_director_name(director_name, movie_data)
 end
 # Your code after this point
 
-ss =  movie_with_director_name("Stephen Spielberg", directors_database)
+ss = movie_with_director_name("Stephen Spielberg", directors_database)
+puts ss
 
 def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
